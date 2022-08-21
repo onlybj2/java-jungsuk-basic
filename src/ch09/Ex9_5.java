55be6e09448f6077d589b1d7deae426955120279
pbj <onlybj2@naver.com>
2022-08-21 23:33:31 +0900

    ch09

---
+++ src/ch09/Ex9_5.java
@@ -0,0 +1,30 @@
+package ch09;
+
+public class Ex9_5 {
+
+    public static void main(String[] args) {
+        Card2 c1 = new Card2();
+        Card2 c2 = new Card2("HEART", 10);
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+    }
+
+}
+
+class Card2 {
+    String kind;
+    int number;
+
+    Card2() {
+        this("SPADE", 1);  // Card(String kind, int number)¸¦ È£Ãâ
+    }
+
+    Card2(String kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public String toString() {
+        return "kind : " + kind + ", number : " + number;
+    }
+}
